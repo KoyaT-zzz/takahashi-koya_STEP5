14eeb04a101e5e4ea137773ebbde59c82845adc9
︎︎ 縛 <oyasumine@noMacBook-Pro.local>
2026-04-23 00:29:36 +0900

    htmlからCSS・JSを分離

---
+++ step5.js
@@ -0,0 +1,78 @@
+const input = document.getElementById("inputText");
+const showBtn = document.getElementById("showBtn");
+const colorBtn = document.getElementById("colorBtn");
+const output = document.getElementById("outputArea");
+const table = document.getElementById("table");
+const body = document.getElementById("body");
+
+let addCount = 0;
+
+//設問1、3〜6
+showBtn.addEventListener("click", function () {
+const text = input.value;
+
+//設問1
+if (text === "") {
+    alert("入力値が空です。");
+    return;
+}
+
+//表示 + ハイライト（設問3）
+output.textContent = text;
+output.classList.toggle("highlight");
+
+//===== 設問4：テーブル最下部に追加 =====
+const row = table.insertRow(-1);
+const cell1 = row.insertCell(0);
+const cell2 = row.insertCell(1);
+
+cell1.textContent = text;
+
+//削除ボタン
+const deleteBtn = document.createElement("button");
+deleteBtn.textContent = "削除";
+
+//===== 設問5：行削除 + カウント減少 =====
+deleteBtn.addEventListener("click", function () {
+    table.deleteRow(row.rowIndex);
+    addCount--;
+
+    if (addCount < 3) {
+        showBtn.style.display = "inline";
+    }
+});
+
+cell2.appendChild(deleteBtn);
+
+addCount++;
+
+//===== 設問6：最大3件制御 =====
+//データ行は「ヘッダー以外」なので rowIndex > 3 で削除
+if (table.rows.length > 4) {
+    table.deleteRow(1); //1行目（最古データ）削除
+    addCount--;
+}
+
+//表示ボタン制御
+if (addCount >= 3) {
+    showBtn.style.display = "none";
+    }
+});
+
+//設問2
+const colors = ["lightblue", "lightgreen", "lightcoral"];
+let index = 0;
+
+colorBtn.addEventListener("click", function () {
+body.style.backgroundColor = colors[index];
+index++;
+
+if (index >= colors.length) {
+    index = 0;
+}
+});
+
+//設問7
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
